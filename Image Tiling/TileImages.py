02f438dcd76d64b31484f63000fbaa60d35e6cc8
Kim2091 <62084776+Kim2091@users.noreply.github.com>
2023-07-24 11:58:40 -0400

    Rewrite
    Cleans up the code, removes unnecessary lines. Retains all functionality

--- Image Tiling/TileImages.py
+++ Image Tiling/TileImages.py
@@ -1,96 +1,73 @@
-import time
-import threading
-from concurrent.futures import ThreadPoolExecutor
-from PIL import Image
 import os
-import math
 import argparse
+from PIL import Image
+from multiprocessing import Pool
+import random
 
-# Set up the command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('folder', help='The folder containing the images')
-parser.add_argument('--tile-size', '-t', type=int, default=512, help='The size of the tiles. Default is 512')
-parser.add_argument('--skip-tiles', '-s', action='store_true', help='Skip tiles that are predominantly black or white')
-parser.add_argument('--min-tile-size', '-m', type=int, help='Limit tile size to a minimum value')
-parser.add_argument('--color-type', '-c', choices=['g', 'c'], help='Output grayscale or color tiles')
-parser.add_argument('output_dir', help='The output directory')
-args = parser.parse_args()
-
-# Create the output directory if it does not already exist
-output_dir = args.output_dir
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-# Get a list of all image files in the folder and its subfolders
-file_list = []
-for root, dirs, files in os.walk(args.folder):
-    for file in files:
-        if file.endswith(('.jpg', '.png', '.gif')):
-            file_list.append(os.path.join(root, file))
-
-# Start the timer
-start_time = time.perf_counter()
-
-# Define a function that processes a single image
-def process_image(file):
-
-    # Load the image
+def process_image(image_path, output_folder, tile_size, num_tiles, grayscale, min_size, skip_black_white):
     try:
-        image = Image.open(os.path.join(args.folder, file))
-    except OSError:
-        print(f'Error: Could not open {file}. Skipping this image.')
-        return
-
-    # Calculate the size of the image
-    width = math.ceil(image.width / args.tile_size) * args.tile_size
-    height = math.ceil(image.height / args.tile_size) * args.tile_size
-
-    # Remove the file extension from the file name
-    file_name, file_extension = os.path.splitext(file)
-
-    # Calculate the number of rows and columns
-    num_rows = math.ceil(image.height / args.tile_size)
-    num_cols = math.ceil(image.width / args.tile_size)
-
-    # Iterate through the rows and columns, and save each tile as a separate image file
-    for row in range(num_rows):
-        for col in range(num_cols):
-            x1 = col * args.tile_size
-            y1 = row * args.tile_size
-            x2 = min(x1 + args.tile_size, image.width)  # Adjust the right edge of the tile if it goes beyond the width of the image
-            y2 = min(y1 + args.tile_size, image.height)  # Adjust the bottom edge of the tile if it goes beyond the height of the image
-
-            tile = image.crop((x1, y1, x2, y2))
-            if tile.width < args.tile_size or tile.height < args.tile_size:
-                continue  # Skip this tile if it is smaller than the specified tile size
-
-            # Convert the tile to grayscale if the output type is set to grayscale
-            if args.color_type == 'g':
-                tile = tile.convert("L")
-
-            # Calculate the average pixel value
-            avg_value = sum(sum(x) for x in tile.getdata()) / len(tile.getdata())
-
-            # Skip the tile if it is predominantly black or white (if the skip_tiles flag is set)
-            if args.skip_tiles and (avg_value < 5 or avg_value > 250):
+        image = Image.open(image_path)
+        width, height = image.size
+        if grayscale:
+            image = image.convert('L')
+        if num_tiles == 0:
+            num_tiles = (width // tile_size[0]) * (height // tile_size[1])
+        tiles_saved = 0
+        for i in range(num_tiles):
+            x = random.randint(0, width - tile_size[0])
+            y = random.randint(0, height - tile_size[1])
+            tile = image.crop((x, y, x + tile_size[0], y + tile_size[1]))
+            if min_size and (tile.width < min_size[0] or tile.height < min_size[1]):
                 continue
+            if skip_black_white:
+                extrema = tile.getextrema()
+                if isinstance(extrema[0], tuple):
+                    is_black = all([x == 0 for x in extrema[0]])
+                    is_white = all([x == 255 for x in extrema[1]])
+                else:
+                    is_black = extrema[0] == 0
+                    is_white = extrema[1] == 255
+                if is_black or is_white:
+                    continue
+            output_path = f"{output_folder}/{os.path.basename(image_path)}_{i}.png"
+            tile.save(output_path)
+            tiles_saved += 1
+        print(f"{tiles_saved} tiles saved from {image_path}")
+    except Exception as e:
+        print(f"Error processing {image_path}: {e}")
 
-            # Skip this tile if it is smaller than the specified minimum tile size (if the min_tile_size flag is set)
-            if args.min_tile_size and (tile.width < args.min_tile_size or tile.height < args.min_tile_size):
-                continue
-               
-            output_file = f'tile_{row}_{col}_{os.path.basename(file_name)}.png'
-            tile.save(os.path.join(output_dir, output_file))
 
 
-with ThreadPoolExecutor() as executor:
-    results = executor.map(process_image, file_list)
+def process_folder(input_folder, output_folder, tile_size, num_tiles, grayscale, min_size, skip_black_white):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    pool = Pool()
+    for root, dirs, files in os.walk(input_folder):
+        for file in files:
+            if file.endswith(('.png', '.jpg', '.webp')):
+                image_path = os.path.join(root, file)
+                pool.apply_async(process_image, (image_path, output_folder, tile_size, num_tiles, grayscale, min_size, skip_black_white))
+    pool.close()
+    pool.join()
 
-# Stop the timer
-end_time = time.perf_counter()
-
-# Calculate the elapsed time
-elapsed_time = end_time - start_time
-
-# Display a confirmation message and the elapsed time
-print(f'Done! Processed {len(file_list)} images in {elapsed_time:.2f} seconds.')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tile images from a folder.')
+    parser.add_argument('input_folder', type=str,
+                        help='Input folder containing images')
+    parser.add_argument('output_folder', type=str,
+                        help='Output folder to save tiles')
+    parser.add_argument('-t', '--tile-size', type=int, nargs=2,
+                        help='Size of tiles (width height)', default=(512, 512))
+    parser.add_argument('-n', '--num-tiles', type=int,
+                        help='Number of tiles to save per image', default=0)
+    parser.add_argument('-g', '--grayscale', action='store_true',
+                        help='Convert tiles to grayscale')
+    parser.add_argument('-m', '--min-size', type=int, nargs=2,
+                        help='Minimum size of tiles to save (width height)')
+    parser.add_argument('-s', '--skip-black-white', action='store_true',
+                        help='Skip black and white tiles')
+    args = parser.parse_args()
+    process_folder(args.input_folder, args.output_folder,
+                   args.tile_size, args.num_tiles,
+                   args.grayscale, args.min_size,
+                   args.skip_black_white)
